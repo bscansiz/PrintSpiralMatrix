098804b849dfd5cec0bb738ba65f2db84c8d796a
bscansiz <97505046+bscansiz@users.noreply.github.com>
2022-09-28 08:34:40 +0300

    Update SpiralMatrixPrint.java

--- SpiralMatrixPrint.java
+++ SpiralMatrixPrint.java
@@ -7,7 +7,7 @@ public class SpiralMatrixPrint {
     public static void main(String[] args) {
 	// write your code here
 
-        Integer[][] matrix = {{1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18},{19,20,21,22,23,24},{25,26,27,28,29,30}};
+        Integer[][] matrix = {{1,2},{7,8},{13,14},{19,20}};
 
         System.out.print(printSprialMatrix(matrix).toString());
 
@@ -35,23 +35,24 @@ public class SpiralMatrixPrint {
 
             int counter = 0;
             for (Integer[] row : matrix) {
-                int columnLength = row.length - 1;
-                if (counter == 0)
-                    firstRow.addAll(Arrays.asList(row));
+                if (row.length > 0) {
+                    int columnLength = row.length - 1;
+                    if (counter == 0)
+                        firstRow.addAll(Arrays.asList(row));
 
-                lastColumn.add(row[columnLength]);
-                firstColumn.add(row[0]);
+                    if (columnLength > 0) lastColumn.add(row[columnLength]);
+                    firstColumn.add(row[0]);
 
-                if (counter == rowLength)
-                    lastRow.addAll(Arrays.asList(row));
+                    if (counter == rowLength)
+                        lastRow.addAll(Arrays.asList(row));
 
-                if (counter != 0 && counter != rowLength) {
-                    if (internalMatrix != null) {
-                        internalMatrix[counter - 1] = Arrays.copyOfRange(row, 1 , columnLength);
+                    if (counter != 0 && counter != rowLength) {
+                        if (internalMatrix != null) {
+                            internalMatrix[counter - 1] = Arrays.copyOfRange(row, 1, columnLength);
+                        }
                     }
+                    counter++;
                 }
-                counter++;
-
             }
 
             Collections.reverse(firstColumn);
