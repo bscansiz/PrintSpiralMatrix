4e1e31886a5248e8ab13299e383b76532dbd39c4
bscansiz <97505046+bscansiz@users.noreply.github.com>
2022-09-28 08:34:08 +0300

    Update SpiralMatrixPrintTest.java

--- SpiralMatrixPrintTest.java
+++ SpiralMatrixPrintTest.java
@@ -1,15 +1,35 @@
-package com.company;
+package test;
 
+import com.company.SpiralMatrixPrint;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SpiralMatrixPrintTest {
-    public static void main(String[] args) {
-        // write your code here
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testPrintSprialMatrix1() {
         Integer[][] matrix = {{1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18},{19,20,21,22,23,24},{25,26,27,28,29,30}};
-        String expectedResult = "1, 2, 3, 4, 5, 6, 12, 18, 24, 30, 29, 28, 27, 26, 25, 19, 13, 7, 8, 9, 10, 11, 17, 23, 22, 21, 20, 14, 15, 16";
-        assertEquals (expectedResult, SpiralMatrixPrint.printSprialMatrix(matrix).toString());
+        String expectedString = "1, 2, 3, 4, 5, 6, 12, 18, 24, 30, 29, 28, 27, 26, 25, 19, 13, 7, 8, 9, 10, 11, 17, 23, 22, 21, 20, 14, 15, 16";
 
+        assertEquals(expectedString, SpiralMatrixPrint.printSprialMatrix(matrix));
+    }
+
+    @Test
+    void testPrintSprialMatrix2() {
+        Integer[][] matrix = {{1,2},{7,8},{13,14},{19,20}};
+        String expectedString = "1, 2, 8, 14, 20, 19, 13, 7";
+
+        assertEquals(expectedString, SpiralMatrixPrint.printSprialMatrix(matrix));
     }
 }
