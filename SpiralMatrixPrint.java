a357a82281ff35167e2a389cb125f17286f5ff19
bscansiz <97505046+bscansiz@users.noreply.github.com>
2022-09-30 15:15:34 +0300

    Update SpiralMatrixPrint.java

--- SpiralMatrixPrint.java
+++ SpiralMatrixPrint.java
@@ -4,80 +4,92 @@ import java.util.*;
 
 public class SpiralMatrixPrint {
 
-    public static void main(String[] args) {
-	// write your code here
-
-        Integer[][] matrix = {{1,2},{7,8},{13,14},{19,20}};
-
-        System.out.print(printSprialMatrix(matrix).toString());
-
-    }
-
+    /**
+     * Prints the spiral traversed matrix
+     * @param matrix
+     * @return
+     */
     public static String printSprialMatrix(Integer[][] matrix) {
         return traverseSprialMatrix(matrix).toString()
                 .replace("[", "")
                 .replace("]", "");
     }
 
+    /**
+     * This method traverses the given matrix as spiral order
+     * and define internal matrix and recursively call itself for internal matrix
+     * @param matrix
+     * @return spiral order of given matrix
+     */
     private static Set<Integer> traverseSprialMatrix(Integer[][] matrix) {
 
         ArrayList<Integer> firstRow = new ArrayList<Integer>();
         ArrayList<Integer> lastRow = new ArrayList<Integer>();
         ArrayList<Integer> lastColumn = new ArrayList<Integer>();
         ArrayList<Integer> firstColumn = new ArrayList<Integer>();
-        Set<Integer> set = new LinkedHashSet<Integer>();
+        Set<Integer> spiralOrderSet = new LinkedHashSet<Integer>();
         int rowLength = matrix.length - 1;
+        int counter = 0;
+
+        //Define an internal matrix
         Integer[][] internalMatrix =  null;
         if (rowLength > 0) {
             internalMatrix = new Integer[rowLength-1][];
         }
 
+        for (Integer[] row : matrix) {
+            //For each row, collect the data
+            if (row.length > 0) {
+                int columnLength = row.length - 1;
 
-            int counter = 0;
-            for (Integer[] row : matrix) {
-                if (row.length > 0) {
-                    int columnLength = row.length - 1;
-                    if (counter == 0)
-                        firstRow.addAll(Arrays.asList(row));
+                //Collect first row items
+                if (counter == 0)
+                    firstRow.addAll(Arrays.asList(row));
 
-                    if (columnLength > 0) lastColumn.add(row[columnLength]);
-                    firstColumn.add(row[0]);
+                //Collect the last column items
+                if (columnLength > 0)
+                    lastColumn.add(row[columnLength]);
 
-                    if (counter == rowLength)
-                        lastRow.addAll(Arrays.asList(row));
+                //Collect the first column items for each row
+                firstColumn.add(row[0]);
 
-                    if (counter != 0 && counter != rowLength) {
-                        if (internalMatrix != null) {
-                            internalMatrix[counter - 1] = Arrays.copyOfRange(row, 1, columnLength);
-                        }
+                //Collect last row items
+                if (counter == rowLength)
+                    lastRow.addAll(Arrays.asList(row));
+
+                //All the other items will be collected as internal matrix
+                if (counter != 0 && counter != rowLength) {
+                    if (internalMatrix != null) {
+                        internalMatrix[counter - 1] = Arrays.copyOfRange(row, 1, columnLength);
                     }
-                    counter++;
                 }
+                counter++;
             }
+        }
 
-            Collections.reverse(firstColumn);
-            Collections.reverse(lastRow);
+        //Reverse order for firstColum and lastRow
+        Collections.reverse(firstColumn);
+        Collections.reverse(lastRow);
 
-            firstRow.addAll(lastColumn);
-            firstRow.addAll(lastRow);
-            firstRow.addAll(firstColumn);
+        //Merge all the collected items in firstRow list
+        firstRow.addAll(lastColumn);
+        firstRow.addAll(lastRow);
+        firstRow.addAll(firstColumn);
 
-            set.addAll(firstRow);
+        //To eliminate the same items, collect items in a set
+        spiralOrderSet.addAll(firstRow);
 
 
-            if (internalMatrix!= null &&
-                    Arrays.deepEquals(internalMatrix, new Integer[rowLength-1][])) {
-                return set;
-            } else {
-                if (internalMatrix == null) {
-                    return set;
-                } else {
-                    set.addAll(traverseSprialMatrix(internalMatrix));
-                }
-            }
+        if ((internalMatrix != null &&
+                Arrays.deepEquals(internalMatrix, new Integer[rowLength-1][]))
+            || internalMatrix == null) {
+            //If there is no item to be traversed anymore, return the collected set
+            return spiralOrderSet;
+        } else {
+            //For internal matrix, call the method itself
+            spiralOrderSet.addAll(traverseSprialMatrix(internalMatrix));
+        }
 
-            return set;
+        return spiralOrderSet;
     }
-
-
 }
